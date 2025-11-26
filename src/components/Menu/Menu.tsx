f6b0ebd3b36618000e1521ebde30237f07876ca1
Emilly <Emilly.esturari@aluno.senai.br>
2025-11-26 17:25:12 -0300

    finalização react parte 2

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,12 @@
+import './Menu.css'
+
+export default function Menu() {
+  return (
+   <div className="cabecalho_menu">
+      <a href="">Nossa casa</a>
+      <a href="">Acompanhamentos</a>
+      <a href="">Bebidas</a>
+      <a href="">Pratos Prontos</a>
+    </div>
+  )
+}
--- src/pages/produtos/Produtos.tsx
+++ src/pages/produtos/Produtos.tsx
@@ -1,18 +1,12 @@
 import './Produtos.css';
-import banner_1 from '../../assets/massa.png';
-import banner_2 from '../../assets/banner 3.png';
-import banner_3 from '../../assets/banner vinho.png';
 import whatsapp from '../../assets/WhatsApp.svg-removebg-preview 2.svg';
-// import card_pepperoni from '../../assets/pizza-de-pepperoni-caseira-portal-minha-receita.webp'
-// import card_brigadeiro from '../../assets/brigadeiro.png'
-// import card_sensacao from '../../assets/Sensação.png'
-// import card_quatro_queijos from '../../assets/quatro-queijo 1.png'
-// import card_frango from '../../assets/Frango.png'
-// import card_choc_mms from '../../assets/Pizza_mms 1.png'
 import { useEffect, useState } from 'react';
 import type { Pizza } from '../../types/Pizza';
 import { getPizzas } from '../../services/PizzaService';
 import CardProduto from '../../components/CardProduto/CardProduto';
+import Carrossel from '../../components/Carrossel/Carrossel';
+import Header from '../../components/Header/Header';
+import { useLocation } from 'react-router-dom';
 
 
 
@@ -23,12 +17,23 @@ export default function Produtos() {
 
 
   const [pizza, setPizzas] = useState<Pizza[]>([]);
+  const location = useLocation();
+  const parametrosPesquisados = new URLSearchParams(location.search);
+  const termoPesquisado = parametrosPesquisados.get('query');
 
   const fetchPizzas = async () => {
     try {
       const dados = await getPizzas();
-      console.log("Dados retornados da API", dados);
-      setPizzas(dados);
+      // console.log("Dados retornados da API", dados);
+      if (termoPesquisado) {
+        const dados_filtrados = dados.filter(p =>
+          p.nome.toLowerCase().includes(termoPesquisado.toLowerCase()) ||
+          p.descricao.toLowerCase().includes(termoPesquisado.toLowerCase()) ||
+          p.categorias.some(cat => cat.toLowerCase().includes(termoPesquisado.toLowerCase()))
+        );
+        setPizzas(dados_filtrados);
+      } else
+        setPizzas(dados);
     } catch (error) {
       console.error("Erro ao executar getPizzas", error)
     }
@@ -37,66 +42,57 @@ export default function Produtos() {
 
   useEffect(() => {
     fetchPizzas();
-  }, [])
+    console.log("termo_pesquisado: ", termoPesquisado);
+  }, [termoPesquisado])
 
 
 
   return (
 
 
+    <>
+      <Header />
 
-    <main className='icone_fundo'>
-
-      <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={banner_1} className="d-block w-100" alt="..." />
+      <main className='icone_fundo'>
+        <Carrossel />
+        {/* <section className="container_produtos"> */}
+          <h1 className="acessivel">Pizzas</h1>
+          <div className="titulo">
+            <span>
+              {
+                termoPesquisado ? `Resultados para: ${termoPesquisado}` : "Nome da categoria"
+              }
+            </span>
+            <hr />
           </div>
-          <div className="carousel-item">
-            <img src={banner_2} className="d-block w-100" alt="..." />
-          </div>
-          <div className="carousel-item">
-            <img src={banner_3} className="d-block w-100" alt="..." />
-          </div>
-        </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
-          data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
-          data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
 
-      <section className="cards" >
+          <section className="cards" >
 
-        {
-          pizza.map((p: Pizza) => (
-            <CardProduto
-              key={p.id}
-              nome={p.nome}
-              descricao={p.descricao}
-              preco={p.preco}
-              imagem={p.imagens[0] ?? ""} //?? usado p caso der erro na img 
+            {
+              pizza.map((p: Pizza) => (
+                <CardProduto
+                  key={p.id}
+                  nome={p.nome}
+                  descricao={p.descricao}
+                  preco={p.preco}
+                  imagem={p.imagens[0] ?? ""} //?? usado p caso der erro na img 
 
-            />
-          ))
-        }
+                />
+              ))
+            }
 
+          </section>
 
+          <section>
 
-
-      </section>
-
-      <a className="whatsapp" href="https://wa.me/5511999999999?text=Olá%20,%20gostaria%20de%20mais%20informações."
-        target="_blank">
-        <img src={whatsapp} alt="icone do whatsapp" />
-      </a>
-
-    </main>
+          <a className="whatsapp" href="https://wa.me/5511999999999?text=Olá%20,%20gostaria%20de%20mais%20informações."
+            target="_blank">
+            <img src={whatsapp} alt="icone do whatsapp" />
+          </a>
+          </section>
+        {/* </section> */}
+      </main >
+    </>
   )
 }
 
